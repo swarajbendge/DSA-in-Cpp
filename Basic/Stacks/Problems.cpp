2597d611153e73e98d817967d98b5c978039f4b2
Swaraj Bendge <133043558+swarajbendge@users.noreply.github.com>
2025-08-23 22:24:59 +0530

    Update Problems.cpp

--- Basic/Stacks/Problems.cpp
+++ Basic/Stacks/Problems.cpp
@@ -3,11 +3,12 @@
 // Popular problems on Stack //
 ///////////////////////////////
 
-// Nearest Greater to Right 
 
+// Nearest Greater to Right 
 vector<int> NGR(vector<int>& nums){
-int n = nums.size();    vector<int> ans(n, -1);
-stack<int> st;
+    int n = nums.size();    
+    vector<int> ans(n, -1);
+    stack<int> st;
 
 for(int i = n - 1; i >= 0; i--){
     while(!st.empty() && st.top() <= nums[i]){
@@ -16,7 +17,66 @@ for(int i = n - 1; i >= 0; i--){
         if(!st.empty()){
             ans[i] = st.top();
         }
-        st.push(ans[i]);
+        st.push(nums[i]);
+    }
+    return ans;
+}
+
+
+// Nearest greater to left
+vector<int> NGL(vector<int>& nums) {
+    int n = nums.size();
+    stack<int> st;
+    vector<int> ans(n, -1);
+
+    for(int i = 0; i < n; i++){
+        while(!st.empty() && st.top() <= nums[i]){
+            st.pop();
+        }
+        if(!st.empty()){
+            ans[i] = st.top();
+        }
+        st.push(nums[i]);
+    }
+    return ans;
+}
+
+
+
+//Nearest smaller to left
+vector<int> NSL(vector<int>& nums) {
+    int n = nums.size();
+    stack<int> st;
+    vector<int> ans(n, -1);
+
+    for(int i = 0; i < n; i++){
+        while(!st.empty() && st.top() >= nums[i]){
+            st.pop();
+        }
+        if(!st.empty()){
+            ans[i] = st.top();
+        }
+        st.push(nums[i]);
+    }
+    return ans;
+}
+
+
+
+//Nearest smaller to right
+vector<int> NSR(vector<int>& nums){
+int n = nums.size();    
+vector<int> ans(n, -1);
+stack<int> st;
+
+for(int i = n - 1; i >= 0; i--){
+    while(!st.empty() && st.top() >= nums[i]){
+            st.pop();
+        }
+        if(!st.empty()){
+            ans[i] = st.top();
+        }   
+        st.push(nums[i]);
     }
     return ans;
 }
