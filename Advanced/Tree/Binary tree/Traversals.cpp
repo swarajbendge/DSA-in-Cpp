7c650cd42f6e826c69a52ecd8c0bc3cf40eb6302
Swaraj Bendge <133043558+swarajbendge@users.noreply.github.com>
2025-09-21 22:43:12 +0530

    Create Traversals.cpp

---
+++ Advanced/Tree/Binary tree/Traversals.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+void inorder(Node* root) {
+    if (root == nullptr) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void preorder(Node* root) {
+    if (root == nullptr) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (root == nullptr) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+
+    cout << "Inorder Traversal: ";
+    inorder(root);
+    cout << "\n";
+
+    cout << "Preorder Traversal: ";
+    preorder(root);
+    cout << "\n";
+
+    cout << "Postorder Traversal: ";
+    postorder(root);
+    cout << "\n";
+
+    return 0;
+}
