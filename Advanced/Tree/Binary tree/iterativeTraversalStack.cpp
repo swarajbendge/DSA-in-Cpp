d689a129b7e63468c3406d7fd9e93917112ef53f
Swaraj Bendge <133043558+swarajbendge@users.noreply.github.com>
2025-09-22 08:44:42 +0530

    Update iterativeTraversalStack.cpp

--- Advanced/Tree/Binary tree/iterativeTraversalStack.cpp
+++ Advanced/Tree/Binary tree/iterativeTraversalStack.cpp
@@ -20,3 +20,26 @@ vector<int> preorderTraversal(TreeNode* root) {
         }
       return ans;
   }
+
+// ** inorder iterative traversal using stack ** //
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL) return ans;
+        TreeNode* node = root;
+        stack<TreeNode*> st;
+        while(true){
+            if(node != NULL) {
+                st.push(node);
+                node = node -> left;
+            }
+            else{
+                if(st.empty() == true) break;
+                node = st.top();
+                st.pop();
+                ans.push_back(node -> val);
+                node = node -> right;
+            }
+        }
+        return ans;
+    }
